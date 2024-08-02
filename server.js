dab1f4e1c9b074403e7baa6ae9d2b6d440f61924
DharmeshV05 <dharmeshbm1@gmail.com>
2024-08-02 23:15:49 +0530

    cors added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { google } = require('googleapis');
 const { GoogleAuth } = require('google-auth-library');
 const multer = require('multer');
@@ -8,9 +9,11 @@ const PDFDocument = require('pdfkit');
 const fs = require('fs');
 const path = require('path');
 
+
 const app = express();
 const PORT = 5000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
