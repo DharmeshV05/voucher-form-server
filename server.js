84a1b718742abcf97d432c07fe7e049042b28f74
DharmeshV05 <dharmeshbm1@gmail.com>
2024-08-04 14:28:01 +0530

    port change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(cors());
