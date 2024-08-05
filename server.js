40d07df861da4b8635ffe9b402cb1cef62c31ed2
Dharmesh Vishwakarma <90438519+DharmeshV05@users.noreply.github.com>
2024-08-05 12:17:22 +0530

    M

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(cors({
-  origin: 'https://voucher-form-frontend.vercel.app/', // Replace with your frontend URL
+  origin: 'http://localhost:3000', // Replace with your frontend URL
   credentials: true,
   optionsSuccessStatus: 200
 }));
