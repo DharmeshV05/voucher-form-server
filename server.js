31d0b126807d51d4faf93457ca00a154efb591d1
Dharmesh Vishwakarma <90438519+DharmeshV05@users.noreply.github.com>
2024-08-08 16:41:48 +0530

    441 date

--- server.js
+++ server.js
@@ -168,6 +168,8 @@ app.post("/submit", upload.none(), async (req, res) => {
     const underlineYPosition = 35; 
 
     doc.fontSize(12).text("Date:", 400, 20);
+
+    doc.fontSize(12).text(voucherData.date, 441, 20);
     doc.fontSize(12).text(voucherData.date, 470, 20);
     doc.fontSize(12).text("Voucher No:", 400, 40);
     doc.fontSize(12).text(voucherData.voucherNo, 470, 40);
